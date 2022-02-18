8d4b2a2683aed705690e6adf8cd43d44bfa6e9c7
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-02-17 23:43:14 -0500

    :lipstick: [ADD] SliderProjects component

---
+++ components/SliderProjects/SliderProjects.jsx
@@ -0,0 +1,54 @@
+import Slider from "react-slick";
+
+export const SliderProjects = ({ slideRef, children }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    className: "Slider",
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider
+      ref={slideRef}
+      {...settings}
+      // style={{ display: "flex", justifyContent: "center", gap: "2rem" }}
+    >
+      {children}
+    </Slider>
+  );
+};
---
+++ components/SliderProjects/index.js
@@ -0,0 +1,1 @@
+export { SliderProjects } from "./SliderProjects";
