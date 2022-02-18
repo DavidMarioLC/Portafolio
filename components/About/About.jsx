d56572d151ca7502ff04913e68481a4da0b6cb49
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-02-17 23:36:37 -0500

    :sparkles: [ADD] About component

---
+++ components/About/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import style from "./About.module.css";
+import { Wrapper, Technologies, Title, DownArrow } from "../index";
+import { motion } from "framer-motion";
+
+export const About = () => {
+  return (
+    <section className={style.about} aria-label="About Me" id="about">
+      <Wrapper>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+              },
+            },
+          }}
+        >
+          <div className={style.content}>
+            <div className={style.resume}>
+              <DownArrow />
+              <Title text="Sobre" highlight="Mí" />
+              <p className={style.text}>
+                Me llamó David Mario Licla nací en la ciudad del eterno sol ☀
+                llamada Ica en Perú.
+                <br />
+                <br />
+                Me fascina el mundo del desarrollo web, sobre todo el Front End.
+                Disfruto mucho del proceso creativo de crear interfaces, así
+                como también me encanta estar en constante aprendizaje y
+                compartir lo que aprendo.
+              </p>
+            </div>
+            <div className={style.stack}>
+              <Title text="Mi" highlight="stack" />
+              <Technologies />
+            </div>
+          </div>
+        </motion.div>
+      </Wrapper>
+    </section>
+  );
+};
