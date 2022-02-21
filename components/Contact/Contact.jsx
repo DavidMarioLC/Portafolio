7491e202c5bc75f9c1f49c35f9c3ac7d6ab19c5e
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-02-21 12:18:28 -0500

    :recycle: [REMOVE] animation

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from "react";
 import style from "./Contact.module.css";
 import { Wrapper, Title, Zizack, Arrow, PatternDots } from "../index";
-import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import confetti from "canvas-confetti";
 import toast, { Toaster } from "react-hot-toast";
@@ -41,90 +40,72 @@ export const Contact = () => {
     <section className={style.contact} aria-label="Contact Me" id="contact">
       <Toaster position="top-center" reverseOrder={false} />
       <Wrapper>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: {
-              scale: 0.8,
-              opacity: 0,
-            },
-            visible: {
-              scale: 1,
-              opacity: 1,
-              transition: {
-                delay: 0.2,
-              },
-            },
-          }}
-        >
-          <div className={style.content}>
-            <div className={style.information}>
-              <Arrow />
-              <Zizack />
-              <Title text="Contactaté" highlight="Conmigo" />
-              <p className={style.description}>
-                Gracias por llegar hasta aquí, si crees que puedo aportar valor
-                a tu equipo, no dudes en escribirme.
-              </p>
-            </div>
-            <div className={style.formWrapper}>
-              <PatternDots />
-              <form className={style.form} ref={formRef} onSubmit={sendMessage}>
-                <div className={style.field}>
-                  <input
-                    className={style.input}
-                    id="mail"
-                    type="text"
-                    placeholder="Correo electrónico"
-                    name="email"
-                    pattern="\S+.*"
-                    required
-                  />
-                  <label className={style.label} htmlFor="mail">
-                    Correo electrónico
-                  </label>
-                </div>
-                <div className={style.field}>
-                  <input
-                    className={style.input}
-                    id="subject"
-                    type="text"
-                    placeholder="Asunto"
-                    pattern="\S+.*"
-                    name="subject"
-                    required
-                  />
-                  <label className={style.label} htmlFor="subject">
-                    Asunto
-                  </label>
-                </div>
-                <div className={style.field}>
-                  <textarea
-                    className={style.input}
-                    pattern="\S+.*"
-                    name="message"
-                    id="message"
-                    cols="30"
-                    rows="10"
-                    placeholder="Mensaje"
-                    required
-                  ></textarea>
-                  <label className={style.label} htmlFor="message">
-                    Mensaje
-                  </label>
-                </div>
-                {loading ? (
-                  <button className={style.buttonLoading} disabled>
-                    Enviando...
-                  </button>
-                ) : (
-                  <button className={style.button}>Enviar mensajito</button>
-                )}
-              </form>
-            </div>
+        <div className={style.content}>
+          <div className={style.information}>
+            <Arrow />
+            <Zizack />
+            <Title text="Contactaté" highlight="Conmigo" />
+            <p className={style.description}>
+              Gracias por llegar hasta aquí, si crees que puedo aportar valor a
+              tu equipo, no dudes en escribirme.
+            </p>
           </div>
-        </motion.div>
+          <div className={style.formWrapper}>
+            <PatternDots />
+            <form className={style.form} ref={formRef} onSubmit={sendMessage}>
+              <div className={style.field}>
+                <input
+                  className={style.input}
+                  id="mail"
+                  type="text"
+                  placeholder="Correo electrónico"
+                  name="email"
+                  pattern="\S+.*"
+                  required
+                />
+                <label className={style.label} htmlFor="mail">
+                  Correo electrónico
+                </label>
+              </div>
+              <div className={style.field}>
+                <input
+                  className={style.input}
+                  id="subject"
+                  type="text"
+                  placeholder="Asunto"
+                  pattern="\S+.*"
+                  name="subject"
+                  required
+                />
+                <label className={style.label} htmlFor="subject">
+                  Asunto
+                </label>
+              </div>
+              <div className={style.field}>
+                <textarea
+                  className={style.input}
+                  pattern="\S+.*"
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  placeholder="Mensaje"
+                  required
+                ></textarea>
+                <label className={style.label} htmlFor="message">
+                  Mensaje
+                </label>
+              </div>
+              {loading ? (
+                <button className={style.buttonLoading} disabled>
+                  Enviando...
+                </button>
+              ) : (
+                <button className={style.button}>Enviar mensajito</button>
+              )}
+            </form>
+          </div>
+        </div>
       </Wrapper>
     </section>
   );
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -5,7 +5,6 @@ import { Wrapper, Rocket } from "../index";
 import Perfil from "../../public/perfil.webp";
 import Focus from "../../public/focus.svg";
 import Target from "../../public/target.svg";
-import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
@@ -13,110 +12,41 @@ export const Hero = () => {
       <Wrapper>
         <div className={style.content}>
           <div className={style.wrapperPhoto}>
-            <motion.div
-              className={style.focus}
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.2,
-                  },
-                },
-              }}
-            >
+            <div className={style.focus}>
               <Image src={Focus} alt="Focus decorate" title="Focus decorate" />
-            </motion.div>
+            </div>
 
-            <motion.div
-              className={style.target}
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.2,
-                  },
-                },
-              }}
-            >
+            <div className={style.target}>
               <Image
                 src={Target}
                 alt="Target decorate"
                 title="Target decorate"
               />
-            </motion.div>
-
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.2,
-                  },
-                },
-              }}
-            >
-              <Image
-                src={Perfil}
-                alt="David Mario Licla"
-                title="David Mario Licla"
-              />
-            </motion.div>
-          </div>
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={{
-              hidden: {
-                scale: 0.8,
-                opacity: 0,
-              },
-              visible: {
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: 0.2,
-                },
-              },
-            }}
-          >
-            <div className={style.information}>
-              <Rocket />
-              <p className={style.name}>David Licla</p>
-              <h1 className={style.title}>
-                <strong className={style.highlightBlue}>Front End</strong>{" "}
-                Engineer
-              </h1>
-              <p className={style.description}>
-                Ser Front End es increíble. No solo creamos interfaces hermosas
-                también creamos experiencias que hacen la vida de las personas
-                más fácil.
-              </p>
-              <a className={style.button} href="#contact">
-                Contactate conmigo
-              </a>
             </div>
-          </motion.div>
+
+            <Image
+              src={Perfil}
+              alt="David Mario Licla"
+              title="David Mario Licla"
+            />
+          </div>
+
+          <div className={style.information}>
+            <Rocket />
+            <p className={style.name}>David Licla</p>
+            <h1 className={style.title}>
+              <strong className={style.highlightBlue}>Front End</strong>{" "}
+              Engineer
+            </h1>
+            <p className={style.description}>
+              Ser Front End es increíble. No solo creamos interfaces hermosas
+              también creamos experiencias que hacen la vida de las personas más
+              fácil.
+            </p>
+            <a className={style.button} href="#contact">
+              Contactate conmigo
+            </a>
+          </div>
         </div>
       </Wrapper>
     </main>
